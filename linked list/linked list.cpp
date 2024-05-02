3a60ec21f5119869fe493fef80c21958067c965f
Putranugroho14 <putranugroho61@gmail.com>
2024-05-02 11:32:57 +0700

    membuat while

--- linked list/linked list.cpp
+++ linked list/linked list.cpp
@@ -37,7 +37,16 @@ void addNode() {
 
         Node* current = START;
         Node* previous = NULL;
+
+
+        while (current != NULL && current->noMhs < newNode->noMhs) {
+            previous = current;
+            current = current->next;
+        }
+
     }
+
+
 }
 
 
